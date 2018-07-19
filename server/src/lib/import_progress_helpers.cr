e03462baea85f826867ff6620337edcbd7b61be1
mikeeus <itsmikias@gmail.com>
2018-07-19 09:46:42 +0300

    Shorten width to 50

--- server/src/lib/import_progress_helpers.cr
+++ server/src/lib/import_progress_helpers.cr
@@ -1,7 +1,7 @@
 require "progress"
 
 module ImportProgressHelpers
-  @bar = ProgressBar.new
+  @bar = ProgressBar.new(total: 50, width: 50)
   @count = 0
 
   # Increment progress bar every 1/100th of the csv's length
@@ -9,7 +9,7 @@ module ImportProgressHelpers
     return unless @show_progress
 
     @count += 1
-    if @count % (@length / 100) == 0
+    if @count % (@length / 50) == 0
       @bar.inc
     end
   end
