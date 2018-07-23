ae46c4efe07722e6fa4eb24c65543091e4b2c236
mikeeus <itsmikias@gmail.com>
2018-07-23 10:04:15 +0300

    Comment out taxes

--- server/src/serializers/annual_total_by_country/show_serializer.cr
+++ server/src/serializers/annual_total_by_country/show_serializer.cr
@@ -7,8 +7,8 @@ class AnnualTotalByCountry::ShowSerializer < Lucky::Serializer
       name: @annual_total_by_country.name,
       short: @annual_total_by_country.short,
       year: @annual_total_by_country.year,
-      totalImportsCents: @annual_total_by_country.total_imports_cents,
-      totalExportsCents: @annual_total_by_country.total_exports_cents,
+      totalImports: @annual_total_by_country.total_imports_cents / 100.0,
+      totalExports: @annual_total_by_country.total_exports_cents / 100.0,
       # totalImportTaxCents: @annual_total_by_country.total_import_tax_cents,
       # totalExportTaxCents: @annual_total_by_country.total_export_tax_cents,
     }
