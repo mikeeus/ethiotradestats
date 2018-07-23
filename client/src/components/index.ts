1b59a8aed23e60024b133941b755a76856d73db5
mikeeus <itsmikias@gmail.com>
2018-07-23 07:31:10 +0300

    Add react-simple-maps and BasicMap component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,8 @@
 import { Dropdown } from './Dropdown';
 import { Header } from './Header/Header';
 import { Layout } from './Layout/Layout';
+import { BasicMap } from './maps/BasicMap';
 import { SearchHscodes } from './SearchHscodes';
 
-export { Layout, Header, Dropdown, SearchHscodes }
+
+export { BasicMap, Layout, Header, Dropdown, SearchHscodes }
